cc32699f568d9b74e69346f65d44c75ca158ad5e
jbrown1899@hotmail.com <jbrown1899@hotmail.com>
2025-08-13 17:38:28 -0400

    first commit

---
+++ index-js.js
@@ -0,0 +1,15 @@
+const connectButton = document.getElementById("connectButton")
+
+// async function main(){
+//     console.log("hello");
+//     let variable = 5;
+//     console.log(variable);
+
+// }
+
+// main()
+//     .then(() => ProcessingInstruction.exit(0))
+//     .catch((error) => {
+//         console.error(error);
+//         ProcessingInstruction.exit(1);
+//     });
