ab0d41165ad488beeec4dcde0ece3ee5542b047a
jbrown1899@hotmail.com <jbrown1899@hotmail.com>
2025-08-13 17:41:31 -0400

    button console logs

--- index-js.js
+++ index-js.js
@@ -1,5 +1,11 @@
 const connectButton = document.getElementById("connectButton")
 
+connectButton.onclick = connect;
+
+function connect() {
+    console.log("hi");
+}
+
 // async function main(){
 //     console.log("hello");
 //     let variable = 5;
